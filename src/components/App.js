4036215d7dbd4b1053c3138a6173063a4d08ea82
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2020-02-12 11:55:25 -0700

    Added redux notes about redux flow in react

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const App = () => {
+    return <div>App</div>
+}
+
+export default App
+
+//call action creator to change state: select song
+//reducers: song list reducer and selected song reducer
+//how do we get action creator and reducer to be called by stateless components?
+//react-redux library connects them together
+//Usisng react-redux, we will create 2 new components: 
+//provider and connect implemented by react-redux
+//after we take reducers and combine them together, we create a store which contains all reducers and state of app
+//take store that gets created, then pass as prop to provider which will be rendered above highest component in hierarchy
+//provider has eternal reference to the sstore: provides info to all different components in app
+//then find every component that needs data in store: song list
+//create an instance of the connect component above the song list
+//connect component (tag) communicates with provider tag at top of hierarchy NOT through
+//props system, through the CONTEXT SYSTEM
+//CONTEXT SYSTEM: allows any parent component to communicate with any child component directly,
+//even if there are components between them
+//connect sends message up to provider which says "tell me whenever list of songs changes"
+//provider sends list of songs to connect which sends as props to songlist component
+//action creators are not stored in store: instead we call an action creator, take action that gets returned and send into store.dispatch fn
+//pass more configuration into connect component: tell connect function that want select song action creator gets sent to songlist component as a prop
+//connect component makes sure state from store and action creator gets passed as props to appropriate component
