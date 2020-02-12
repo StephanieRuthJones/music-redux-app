731ff38256891d148d078186aab8fead7e9dafd9
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2020-02-12 12:15:32 -0700

    In App.js, imported Provider and createStore from react-redux and redux. Imported reducers. Wrapped <App> component with Provider. Passed store as prop that referenced createStore fn that took in reducers as an argument.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+//group 3rd party dependencies
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+//group own code: 
 import App from './components/App'
+import reducers from './reducers'
 
+//need provider tag at top of component hierarchy
 ReactDOM.render(
-    <App />,
+    <Provider store={createStore(reducers)}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 )
