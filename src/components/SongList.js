14fa320e2a96f7e008e8aa213d361552543d7a4a
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2020-02-12 12:53:51 -0700

    hooked selectSong to redux and connected to onClick

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { selectSong } from '../actions'
 
 class SongList extends Component {
     renderList() {
-        return this.props.songs.map((song) => {
+        return this.props.songs.map(song => {
             return (
                 <div className="item" key={song.title}>
                     <div className="right floated content">
-                        <button className="ui button primary">
+                        <button
+                            className="ui button primary"
+                            onClick={() => this.props.selectSong(song)}
+                        >
                             Select
                         </button>
                     </div>
@@ -21,7 +25,7 @@ class SongList extends Component {
 
     render() {
         //we get songList and dispatch fn as props
-        // console.log(this.props)
+        console.log(this.props)
         return (
             <div className="ui divided list">{this.renderList()}</div>
         )
@@ -43,9 +47,23 @@ class SongList extends Component {
 //that will cause it to show up as props in our component
 //could call it "get my state", convention === name as below
 const mapStateToProps = state => {
+    //see redux store state: 
+    console.log("store state", state)
     //object that we return will show up as props inside our component
     return { songs: state.songs }
 }
+//redux code requires a lot of boilerplate code (in comparison w other state management libraries)
 //pass mapStateToProps to connect fn
-export default connect(mapStateToProps)(SongList)
+//import selectSong and pass into connect fn
+//don't call directly bc redux does not automatically detect action creators being called
+//redux does not automatically detect a fn returning an object that is called an action
+//nothing about action creator fn that automatically wires fn to redux
+//when inside SongList and import action creator fn, we are just importing regular JS fn, will be treated as normal fn call
+//needs to be put into store.dispatch() fn, so can't just call fn
+//need to take returned action and put into dispatch fn
+//when pass action creators into connect fn, connect fn does special operation on the fns inside of the object: 
+//looks at all fns passed in and wraps them in another JS function
+//call action creator, takes returned action, then calls dispatch fn for us
+//by passing selectSong into connect, takes returned action into dispatch
+export default connect(mapStateToProps, { selectSong })(SongList)
 
